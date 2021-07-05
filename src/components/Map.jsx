6fc7001ded7674ff3d6584f5c44f2a8beb83bc16
petartodorovski <petar.todorovski@newsroom.tech>
2021-07-05 10:48:54 +0200

    map issues fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,7 @@ import citiesData from '../data/bg.json'
 
 // Map
 function Map() {
-  const API_KEY = '6f287b6d54bee8794a41d7776558222c'
+  const API_KEY = 'f20381ae1fbc84fffcd2a4865eeab471'
   // Selected city state
   const [selectedCity, setSelectedCity] = useState(null)
   const [data, setData] = useState({
@@ -13,67 +13,67 @@ function Map() {
     humidity: '',
     pressure: '',
     feelsLike: '',
-    // icon: ''
+    daily: ''
   })
   const [icon, setIcon] = useState({})
   
   const fetchCurrentWeather =(city)=> {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${(city.lat)}&lon=${(city.lng)}&appid=${API_KEY}&units=metric`)
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${(city.lat)}&lon=${(city.lng)}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
     .then(res=> {
-      console.log(res.data, 'dataaaaa')
       setData({...data, 
-        temperature: res.data.main.temp,
-        humidity: res.data.main.humidity,
-        pressure: res.data.main.pressure,
-        feelsLike: res.data.main.feels_like
+        temperature: res.data.current.temp,
+        humidity: res.data.current.humidity,
+        pressure: res.data.current.pressure,
+        feelsLike: res.data.current.feels_like,
+        daily: res.data.daily
       })
     })
-    console.log(data.temperature, 'tamparatumparaaaaaaa')
   }
 
   useEffect(() => {
     citiesData.map((city)=> {
       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${(city.lat)}&lon=${(city.lng)}&appid=6f287b6d54bee8794a41d7776558222c&units=metric`)
       .then(res=> {
-        // setData({...data, icon: res.data.weather[0].icon})
         setIcon(res.data.weather[0].icon)
       })
     })
   }, [])
 
-  console.log(icon, 'iconnnnnnnnn nadvorrrr skrosss')
-
-  return <GoogleMap
-    defaultZoom={7}
-    defaultCenter={{ lat: 42.733883, lng: 25.485830 }}>
-    {citiesData.map((city) => (
-      <Marker
-        key={city.id}
-        position={{ lat: parseFloat(city.lat), lng: parseFloat(city.lng) }}
-        onClick={()=> {
-          setSelectedCity(city);
-          fetchCurrentWeather(city);
-        }}
-        // icon={{url: `http://openweathermap.org/img/wn/${icon}@2x.png` }}
-      />
-    ))}
-    {selectedCity && (
-      <InfoWindow 
-        position={{ lat: parseFloat(selectedCity.lat), lng: parseFloat(selectedCity.lng) }}
-        onCloseClick={()=> {
-          setSelectedCity(null)
-        }}
-      >
-        <ul style={{textAlign: 'left'}}>
-          <li>Temperature: {data.temperature}&deg;</li>
-          <li>Humidity: {data.humidity}%</li>
-          <li>pressure: {data.pressure}hPa</li> 
-          <li>Feels like: {data.feelsLike}&deg;</li>
-          <li>Icon: {icon}</li>
-        </ul>
-      </InfoWindow>
-    )}
-  </GoogleMap>
+  return (
+    <div>
+      <GoogleMap
+      defaultZoom={7}
+      defaultCenter={{ lat: 42.733883, lng: 25.485830 }}>
+      {citiesData.map((city) => (
+        <Marker
+          key={city.id}
+          position={{ lat: parseFloat(city.lat), lng: parseFloat(city.lng) }}
+          onClick={()=> {
+            setSelectedCity(city);
+            fetchCurrentWeather(city);
+          }}
+          // icon={{url: `http://openweathermap.org/img/wn/${icon}@2x.png` }}
+        />
+      ))}
+      {selectedCity && (
+        <InfoWindow 
+          position={{ lat: parseFloat(selectedCity.lat), lng: parseFloat(selectedCity.lng) }}
+          onCloseClick={()=> {
+            setSelectedCity(null)
+          }}
+        >
+          <ul style={{textAlign: 'left'}}>
+            <li>Temperature: {data.temperature}&deg;</li>
+            <li>Humidity: {data.humidity}%</li>
+            <li>pressure: {data.pressure}hPa</li> 
+            <li>Feels like: {data.feelsLike}&deg;</li>
+            <li>Icon: {icon}</li>
+          </ul>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  </div>
+  )
 }
 
 // HOF WrappedMap
